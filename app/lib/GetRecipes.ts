4d4a0e95605064a0c02d02e8816f91ed8d82435f
Junaid7200 <junaidjaffery1@gmail.com>
2025-11-08 16:32:16 +0500

    fix: wrong placement of caching command fixed

--- app/lib/GetRecipes.ts
+++ app/lib/GetRecipes.ts
@@ -18,8 +18,8 @@ export async function getRandomRecipeCards(
         headers: {
           "x-rapidapi-key": process.env.RAPIDAPI_KEY!,
           "x-rapidapi-host": HOST,
+          cache: "no-store",
         },
-        cache: "no-store",
       }
     );
   
